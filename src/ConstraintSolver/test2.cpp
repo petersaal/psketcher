37a0a2a5a01deec75d2fe6462ea7542649517fde
grem0009 <grem0009@26dfb9b6-6b4c-0410-b2d4-4f28753049b9>
2007-02-25 20:19:41 +0000

    Initial import.

---
+++ src/ConstraintSolver/test2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include "ConstraintSolver.h"
+#include "ModelWorkspace.h"
+#include "../mmcMatrix/mmcMatrix.h"
+
+using namespace std;
+
+int main(void)
+{
+	Point point1(0.0,0.0,0.0);
+	Point point2(10.0,0.0,0.0);
+	Point point3(10.0,10.0,0.0);
+	Point point4(0.0,10.0,0.0);	
+
+	Line line1(point1,point2);
+	Line line2(point2,point3);
+	Line line3(point3,point4);
+	Line line4(point4,point1);
+
+	line1.Draw();
+	line2.Draw();
+	line3.Draw();
+	line4.Draw();
+
+	return 0;
+}
+
