c323adfede56200ec29e73e91b1b5355c1d22a14
grem0009 <grem0009@26dfb9b6-6b4c-0410-b2d4-4f28753049b9>
2007-09-01 19:54:37 +0000

    Implemented DependentDOF class. The constraint equation base class needs to be updated to support dependent DOF's.

--- trunk/src/ConstraintSolver/Primitives.cpp
+++ trunk/src/ConstraintSolver/Primitives.cpp
@@ -15,6 +15,63 @@ DOF(name,free,false)
 	value_ = value;
 }
 
+DependentDOF :: DependentDOF (double expression, std::vector<DOFPointer> source_dof_list):
+DOF(false,true)
+{
+	// @fixme Need to make sure that all DOF's in expression are included in the DOF list
+	expression_ = expression;
+	source_dof_list_ = source_dof_list;
+}
+
+DependentDOF :: DependentDOF ( const char *name, double expression, std::vector<DOFPointer> source_dof_list):
+DOF(name,false,true)
+{
+	// @fixme Need to make sure that all DOF's in expression are included in the DOF list
+	expression_ = expression;
+	source_dof_list_ = source_dof_list;
+}
+
+double DependentDOF::GetValue()const
+{
+	// For each independent DOF in the list, substitute its value into the expression that defines this dependent DOF
+	GiNaC::ex current_expression = GetExpression();
+	double result;
+
+	for(unsigned int current_dof = 0; current_dof < source_dof_list_.size(); current_dof++)
+	{
+		if(!source_dof_list_[current_dof]->IsDependent())
+		{
+			current_expression = current_expression.subs(source_dof_list_[current_dof]->GetVariable() ==
+														 source_dof_list_[current_dof]->GetValue(),subs_options::no_pattern);
+		}
+	}
+
+	// Now evaluate the expression to a numeric value
+	// check to make sure the expression evaluates to a numeric value
+	if (is_a<numeric>(current_expression)) {
+		result = ex_to<numeric>(current_expression).to_double();
+	} else {
+		throw PrimitiveException();
+  	}
+
+	return result;
+}
+
+GiNaC::ex DependentDOF::GetExpression()const
+{
+	// For each dependent DOF in the list, substitute its expression into the overall expression that defines this dependent DOF
+	GiNaC::ex result = expression_;
+
+	for(unsigned int current_dof = 0; current_dof < source_dof_list_.size(); current_dof++)
+	{
+		if(source_dof_list_[current_dof]->IsDependent())
+		{
+			result = result.subs(source_dof_list_[current_dof]->GetVariable() == source_dof_list_[current_dof]->GetExpression(),subs_options::no_pattern);
+		}
+	}
+
+	return result;
+}
 
 Point :: Point ( double x, double y, double z, bool x_free, bool y_free, bool z_free):
 x_(new IndependentDOF(x,x_free)),
