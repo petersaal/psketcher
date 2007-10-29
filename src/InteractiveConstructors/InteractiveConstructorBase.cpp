b69289bd345f3905219009d286c6dfab9dbcc322
grem0009 <grem0009@26dfb9b6-6b4c-0410-b2d4-4f28753049b9>
2007-10-29 01:36:47 +0000

    Initial skeleton for interactively creating 2D sketch primitives added.

---
+++ src/InteractiveConstructors/InteractiveConstructorBase.cpp
@@ -0,0 +1,8 @@
+#include "InteractiveConstructorBase.h"
+
+InteractiveConstructorBase::InteractiveConstructorBase(OccSketchPointer parent_sketch, Handle(V3d_Viewer) current_viewer):
+parent_sketch_(parent_sketch),
+current_viewer_(current_viewer)
+{
+
+}
