3f5b4f241a02bfc6f3eb4ce2fa93fcdebd5a6bfa
grem0009 <grem0009@26dfb9b6-6b4c-0410-b2d4-4f28753049b9>
2007-11-04 05:04:54 +0000

    Further work on the interactive primitive constructors.

--- trunk/src/InteractiveConstructors/InteractiveConstructorBase.h
+++ trunk/src/InteractiveConstructors/InteractiveConstructorBase.h
@@ -1,18 +1,21 @@
 #ifndef InteractiveConstructorBaseH
 #define InteractiveConstructorBaseH
 
-enum MouseButton {LeftButton, MiddleButton, RightButton, Button4, Button5};
+#include <boost/shared_ptr.hpp>
+
+#include <V3d_Viewer.hxx>
+
+#include "../OpenCascadeBinding/OccSketch.h"
+
+enum MouseButton {LeftButton, MiddleButton, RightButton, Button4, Button5, OtherButton};
 enum MouseButtonEventType {ButtonPress, ButtonRelease};
 
 class MouseEventProperties
 {
 	public:
-		// Constructor (there is no default constructor, each binding will need to define its own)
-		MouseEventProperties()=0;
-
 		// Accessor methods used to retrieve values (the constructor will define all of the values)
-		double GetXPosition() {return x_position;}
-		double GetYPosition() {return y_position;}
+		double GetXPosition() {return x_position_;}
+		double GetYPosition() {return y_position_;}
 		
 		bool ControlPressed() {return control_;}
 		bool ShiftPressed() {return shift_;}
@@ -34,32 +37,42 @@ class MouseEventProperties
 };
 typedef boost::shared_ptr<MouseEventProperties> MouseEventPropertiesPointer;
 
-// Get binding for the mouse event properties (only the constructor needs to be defined) 
-class GtkMouseEventProperties : public MouseEventProperties
-{
-	// Constructor
-	GtkMouseEventProperties(GdkEventButton  *event);
-};
-typedef boost::shared_ptr<GtkMouseEventProperties> GtkMouseEventPropertiesPointer;
+
 
 class MotionEventProperties
 {
 	public:
-		// Constructor (there is no default constructor, each binding will need to define its own)
-		MotionEventProperties()=0;
-
 		// Accessor methods used to retrieve values (the constructor will define all of the values)
-	private:
+		double GetXPosition() {return x_position_;}
+		double GetYPosition() {return y_position_;}
+		
+		bool ControlPressed() {return control_;}
+		bool ShiftPressed() {return shift_;}
+		bool AltPressed() {return alt_;}
+
+		bool LeftButtonPressed() {return left_button_;}
+		bool MiddleButtonPressed() {return middle_button_;}
+		bool RightButtonPressed() {return right_button_;}
+		bool Button4Pressed() {return button_4_;}
+		bool Button5Pressed() {return button_5_;}
+
+	protected:
+		double x_position_;
+		double y_position_;
+		
+		bool control_;
+		bool shift_;
+		bool alt_;
+
+		bool left_button_;
+		bool middle_button_;
+		bool right_button_;
+		bool button_4_;
+		bool button_5_;
 };
 typedef boost::shared_ptr<MotionEventProperties> MotionEventPropertiesPointer;
 
-// Get binding for the mouse event properties (only the constructor needs to be defined) 
-class GtkMotionEventProperties : public MotionEventProperties
-{
-	// Constructor
-	GtkMotionEventProperties(GdkEventMotion  *event);
-};
-typedef boost::shared_ptr<GtkMotionEventProperties> GtkMotionEventPropertiesPointer;
+
 
 
 class InteractiveConstructorBase
@@ -70,19 +83,19 @@ class InteractiveConstructorBase
 		virtual ~InteractiveConstructorBase() {;}
 		
 		// all derived classes must implement this method
-		void CreateObject()=0; // Adds the created object to parent_sketch_
+		virtual void CreateObject()=0; // Adds the created object to parent_sketch_
 
 		// GUI event methods
 		// each method returns true if the object being created is completely defined and false otherwise
-		bool LeftButtonDown(MouseEventPropertiesPointer event_props) {return false;}
-		bool MiddleButtonDown(MouseEventPropertiesPointer event_props) {return false;}
-		bool MiddleButtonDown(MouseEventPropertiesPointer event_props) {return false;}
+		virtual bool LeftButtonDown(MouseEventPropertiesPointer event_props) {return false;}
+		virtual bool MiddleButtonDown(MouseEventPropertiesPointer event_props) {return false;}
+		virtual bool RightButtonDown(MouseEventPropertiesPointer event_props) {return false;}
 
-		bool LeftButtonUp(MouseEventPropertiesPointer event_props) {return false;}
-		bool MiddleButtonUp(MouseEventPropertiesPointer event_props) {return false;}
-		bool MiddleButtonUp(MouseEventPropertiesPointer event_props) {return false;}
+		virtual bool LeftButtonUp(MouseEventPropertiesPointer event_props) {return false;}
+		virtual bool MiddleButtonUp(MouseEventPropertiesPointer event_props) {return false;}
+		virtual bool RightButtonUp(MouseEventPropertiesPointer event_props) {return false;}
 
-		bool MouseMove(MoveEventPropertiesPointer event_props) {return false;}
+		virtual bool MouseMove(MotionEventPropertiesPointer event_props) {return false;}
 
 	private:
 		OccSketchPointer parent_sketch_;
