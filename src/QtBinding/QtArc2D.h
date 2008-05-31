d598f59470d4c7b610f740b008755f02428e6e4a
grem0009 <grem0009@26dfb9b6-6b4c-0410-b2d4-4f28753049b9>
2008-05-31 03:56:39 +0000

    QtArc2D now displays the radius text and the text can be edited in place. The radius arrow now needs to be updated so that it follows the location of the radius text.

--- src/QtBinding/QtArc2D.h
+++ src/QtBinding/QtArc2D.h
@@ -1,10 +1,16 @@
 #ifndef QtArc2DH
 #define QtArc2DH
 
+#include <boost/enable_shared_from_this.hpp>
+
+#include <QGraphicsProxyWidget>
 #include "QtPrimitiveBase.h"
 
+class QLineEdit;
+class QtArc2DWidget;
+
 // line class
-class QtArc2D : public QtPrimitiveBase, public Arc2D
+class QtArc2D : public QtPrimitiveBase, public Arc2D, public boost::enable_shared_from_this<QtArc2D>
 {
 	public:
 		QtArc2D (QGraphicsItem * parent, double s_center, double t_center, double theta_1, double theta_2, double radius, 
@@ -22,10 +28,39 @@ class QtArc2D : public QtPrimitiveBase, public Arc2D
 		virtual void paint(QPainter*, const QStyleOptionGraphicsItem*, QWidget*);
 
 	private:
+		QtArc2DWidget *radius_widget_;
 
 };
 typedef boost::shared_ptr<QtArc2D> QtArc2DPointer;
 
 
+// class defining the line edit graphics item used by QtDistancePoint2D
+class QtArc2DWidget : public QGraphicsProxyWidget
+{
+	Q_OBJECT
+
+	public:
+		QtArc2DWidget(QtArc2DPointer arc_primitive, QGraphicsItem *parent = 0);
+		virtual void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget * widget);
+		virtual bool event(QEvent *event);
+
+		void UpdateGeometry(double scale);
+
+
+	signals:
+		void modelChanged();  // emitted when changes are applied
+
+	private slots:
+		void textChanged();
+		void applyChanges();
+
+	private:
+		// methods
+
+		// Parameters
+		QtArc2DPointer arc_primitive_; 
+
+		QLineEdit *radius_line_edit_;
+};
 
 #endif //QtArc2DH
