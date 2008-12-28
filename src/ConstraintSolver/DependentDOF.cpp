f7b15af12ca9955994d1c4d86eeacbc9553af71f
grem0009 <grem0009@26dfb9b6-6b4c-0410-b2d4-4f28753049b9>
2008-12-28 20:09:27 +0000

    DependentDOF can now add itself to the datbase. Still need to create the dependent_dof_list table for each instance of DependentDOF.

--- src/ConstraintSolver/DependentDOF.cpp
+++ src/ConstraintSolver/DependentDOF.cpp
@@ -8,7 +8,7 @@ using namespace std;
 using namespace GiNaC;
 
 
-const string SQL_dependent_dof_database_schema = "CREATE TABLE dependent_dof_list (id INTEGER PRIMARY KEY, variable_name TEXT NOT NULL, bool_free INTEGER NOT NULL, expression TEXT NOT NULL, source_dof_table_name TEXT NOT NULL);";
+const string SQL_dependent_dof_database_schema = "CREATE TABLE dependent_dof_list (id INTEGER PRIMARY KEY, variable_name TEXT NOT NULL, expression TEXT NOT NULL, source_dof_table_name TEXT NOT NULL);";
 
 DependentDOF :: DependentDOF (ex expression, std::vector<DOFPointer> source_dof_list):
 DOF(false /*free*/,true /*dependent*/)
@@ -71,5 +71,65 @@ GiNaC::ex DependentDOF::GetExpression()const
 // method for adding this object to the SQLite3 database
 void DependentDOF::AddToDatabase(sqlite3 *database)
 {
-	
+	// set the database for this object, in the future this database will be updated whenever this object is updated
+	database_ = database;
+
+	// First, create the sql statements to undo and redo this operation
+	stringstream temp_stream;
+	temp_stream.precision(__DBL_DIG__);
+	temp_stream << "BEGIN; "
+                << "INSERT INTO dependent_dof_list VALUES(" 
+                << id_number_ << ",'" << variable_.get_name() << "','" 
+				<< expression_ << "', 'source_dof_table_" << id_number_ <<"'); "
+                << "INSERT INTO dof_list VALUES("
+                << id_number_ << ",'dependent_dof_list'); "
+                << "COMMIT; ";
+
+	string sql_insert = temp_stream.str();
+
+	temp_stream.str(""); // clears the string stream
+
+	temp_stream << "BEGIN; "
+				<< "DELETE FROM dof_list WHERE id=" << id_number_ 
+				<< "; DELETE FROM dependent_dof_list WHERE id=" << id_number_ 
+				<< "; COMMIT;";
+
+	string sql_undo = temp_stream.str();
+
+	// add this object to the appropriate tables by executing the SQL command sql_insert 
+	char *zErrMsg = 0;
+	int rc = sqlite3_exec(database_, sql_insert.c_str(), 0, 0, &zErrMsg);
+	if( rc!=SQLITE_OK ){
+		std::cerr << "SQL error: " << zErrMsg << endl;
+		sqlite3_free(zErrMsg);
+		
+		// the table "dependent_dof_list" may not exist, attempt to create
+		rc = sqlite3_exec(database_, ("ROLLBACK;"+SQL_dependent_dof_database_schema).c_str(), 0, 0, &zErrMsg);  // need to add ROLLBACK since previous transaction failed
+		if( rc!=SQLITE_OK ){
+			std::string error_description = "SQL error: " + std::string(zErrMsg);
+			sqlite3_free(zErrMsg);
+			throw Ark3DException(error_description);
+		}
+
+		// now that the table has been created, attempt the insert one last time
+		rc = sqlite3_exec(database_, sql_insert.c_str(), 0, 0, &zErrMsg);
+		if( rc!=SQLITE_OK ){
+			std::string error_description = "SQL error: " + std::string(zErrMsg);
+			sqlite3_free(zErrMsg);
+			throw Ark3DException(error_description);
+		}
+	}
+
+	// finally, update the undo_redo_list in the database with the database changes that have just been made
+	// need to use sqlite3_mprintf to make sure the single quotes in the sql statements get escaped where needed
+	char *sql_undo_redo = sqlite3_mprintf("INSERT INTO undo_redo_list(undo,redo) VALUES('%q','%q')",sql_undo.c_str(),sql_insert.c_str());
+
+	rc = sqlite3_exec(database_, sql_undo_redo, 0, 0, &zErrMsg);
+	if( rc!=SQLITE_OK ){
+		std::string error_description = "SQL error: " + std::string(zErrMsg);
+		sqlite3_free(zErrMsg);
+		throw Ark3DException(error_description);
+	}
+
+	sqlite3_free(sql_undo_redo);
 }
