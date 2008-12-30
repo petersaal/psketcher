e4952f125dfc13a03dfec2f942ad2cc0f5934b79
grem0009 <grem0009@26dfb9b6-6b4c-0410-b2d4-4f28753049b9>
2008-12-30 06:44:13 +0000

    Added copyright/license headers to all of the source files.

--- trunk/src/ConstraintSolver/ConstraintSolver.cpp
+++ trunk/src/ConstraintSolver/ConstraintSolver.cpp
@@ -1,3 +1,19 @@
+/****************************************************************************
+**
+** This file is part of the pSketcher project.
+**
+** This file may be used under the terms of the GNU General Public
+** License version 2.0 as published by the Free Software Foundation
+** and appearing in the file LICENSE.GPL included in the packaging of
+** this file.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+** Copyright (C) 2006-2008 Michael Greminger. All rights reserved.
+**
+****************************************************************************/
+
 #include <math.h>
 #include "ConstraintSolver.h"
 
