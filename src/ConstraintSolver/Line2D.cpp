d9c4632d53e4a7645f5806d063159219ca3e5cac
grem0009 <grem0009@26dfb9b6-6b4c-0410-b2d4-4f28753049b9>
2008-12-31 03:33:51 +0000

    Database add and remove implemented for Line and Line2D.

--- src/ConstraintSolver/Line2D.cpp
+++ src/ConstraintSolver/Line2D.cpp
@@ -14,7 +14,11 @@
 **
 ****************************************************************************/
 
+#include <sstream>
+
 #include "Line2D.h"
+
+const std::string SQL_line2d_database_schema = "CREATE TABLE line2d_list (id INTEGER PRIMARY KEY, dof_table_name TEXT NOT NULL, primitive_table_name TEXT NOT NULL, sketch_plane INTEGER NOT NULL, s1_dof INTEGER NOT NULL, t1_dof INTEGER NOT NULL, s2_dof INTEGER NOT NULL, t2_dof INTEGER NOT NULL);";
 
 using namespace std;
 
@@ -98,4 +102,102 @@ Point2DPointer Line2D::GetPoint2()
 	// create the actual point object
 	Point2DPointer result(new Point2D(GetS2(), GetT2(), GetSketchPlane()));
 	return result;
+}
+
+void Line2D::AddToDatabase(sqlite3 *database)
+{
+	database_ = database;
+	DatabaseAddRemove(true);
+}
+
+void Line2D::RemoveFromDatabase()
+{
+	DatabaseAddRemove(false);
+}
+
+void Line2D::DatabaseAddRemove(bool add_to_database) // Utility method used by AddToDatabase and RemoveFromDatabase
+{
+	string sql_do, sql_undo;
+
+	stringstream dof_list_table_name;
+	dof_list_table_name << "dof_table_" << GetID();
+	stringstream primitive_list_table_name;
+	primitive_list_table_name << "primitive_table_" << GetID();
+
+	// First, create the sql statements to undo and redo this operation
+	stringstream temp_stream;
+	temp_stream.precision(__DBL_DIG__);
+	temp_stream << "BEGIN; "
+                << "INSERT INTO line2d_list VALUES(" 
+                << GetID() << ",'" << dof_list_table_name.str() << "','" 
+				<< primitive_list_table_name.str() << "'," << GetSketchPlane()->GetID() 
+				<< "," << s1_->GetID() << "," << t1_->GetID()
+				<< "," << s2_->GetID() << "," << t2_->GetID() << "); "
+                << "INSERT INTO primitive_list VALUES("
+                << GetID() << ",'line2d_list'); "
+                << "COMMIT; ";
+
+	if(add_to_database)
+		sql_do = temp_stream.str();
+	else
+		sql_undo = temp_stream.str();
+
+	temp_stream.str(""); // clears the string stream
+
+	temp_stream << "BEGIN; "
+				<< "DELETE FROM primitive_list WHERE id=" << GetID() 
+				<< "; DELETE FROM line2d_list WHERE id=" << GetID() 
+				<< "; COMMIT;";
+
+	if(add_to_database)
+		sql_undo = temp_stream.str();
+	else
+		sql_do = temp_stream.str();
+
+	// add this object to the appropriate tables by executing the SQL command sql_insert 
+	char *zErrMsg = 0;
+	int rc = sqlite3_exec(database_, sql_do.c_str(), 0, 0, &zErrMsg);
+	if( rc!=SQLITE_OK ){
+		if(add_to_database)
+		{
+			//std::cerr << "SQL error: " << zErrMsg << endl;
+			sqlite3_free(zErrMsg);
+			
+			// the table "independent_dof_list" may not exist, attempt to create
+			rc = sqlite3_exec(database_, ("ROLLBACK;"+SQL_line2d_database_schema).c_str(), 0, 0, &zErrMsg);  // need to add ROLLBACK since previous transaction failed
+			if( rc!=SQLITE_OK ){
+				std::string error_description = "SQL error: " + std::string(zErrMsg);
+				sqlite3_free(zErrMsg);
+				throw Ark3DException(error_description);
+			}
+	
+			// now that the table has been created, attempt the insert one last time
+			rc = sqlite3_exec(database_, sql_do.c_str(), 0, 0, &zErrMsg);
+			if( rc!=SQLITE_OK ){
+				std::string error_description = "SQL error: " + std::string(zErrMsg);
+				sqlite3_free(zErrMsg);
+				throw Ark3DException(error_description);
+			}
+		} else {
+			std::string error_description = "SQL error: " + std::string(zErrMsg);
+			sqlite3_free(zErrMsg);
+			throw Ark3DException(error_description);
+		}
+	}
+
+	// finally, update the undo_redo_list in the database with the database changes that have just been made
+	// need to use sqlite3_mprintf to make sure the single quotes in the sql statements get escaped where needed
+	char *sql_undo_redo = sqlite3_mprintf("INSERT INTO undo_redo_list(undo,redo) VALUES('%q','%q')",sql_undo.c_str(),sql_do.c_str());
+
+	rc = sqlite3_exec(database_, sql_undo_redo, 0, 0, &zErrMsg);
+	if( rc!=SQLITE_OK ){
+		std::string error_description = "SQL error: " + std::string(zErrMsg);
+		sqlite3_free(zErrMsg);
+		throw Ark3DException(error_description);
+	}
+
+	sqlite3_free(sql_undo_redo);
+
+	// Now use the method provided by PrimitiveBase to create the tables listing the DOF's and the other Primitives that this primitive depends on
+	DatabaseAddDeleteLists(add_to_database,dof_list_table_name.str(),primitive_list_table_name.str());
 }
