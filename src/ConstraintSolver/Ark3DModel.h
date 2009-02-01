ec31244757770fd06348e9ff6a43df02d9f4f3da
grem0009 <grem0009@26dfb9b6-6b4c-0410-b2d4-4f28753049b9>
2009-02-01 03:53:15 +0000

    Get dof_list and primitive_list from database implemented for PrimitiveBase class.

--- src/ConstraintSolver/Ark3DModel.h
+++ src/ConstraintSolver/Ark3DModel.h
@@ -97,4 +97,60 @@ private:
 };
 
 
+// Must define the template member functions in the header file since it won't work to define them in Ark3DModel.cpp
+template <class data_t> boost::shared_ptr<data_t> Ark3DModel::FetchPrimitive(unsigned id)
+{
+	PrimitiveBasePointer temp;
+	boost::shared_ptr<data_t> result;
+
+	std::map<unsigned,PrimitiveBasePointer>::iterator primitive_it = primitive_list_.find(id);
+	if(primitive_it != primitive_list_.end())
+	{
+		// primitive already exists
+		temp = primitive_it->second;
+
+	} else {
+		// primitive object does not exist, need to create it from the database
+		temp = PrimitiveFactory(id);
+		AddPrimitive(temp);
+	}
+
+	// attempt to cast the PrimitiveBasePointer to the desired datatype, generate an error if the cast fails
+	if(dynamic_cast<data_t *>(temp.get()) != 0){
+		result = boost::dynamic_pointer_cast<data_t>(temp);
+	} else {
+		throw Ark3DException("Requested data type does not match database data type.");
+	}
+	
+	return result;
+}
+
+template <class data_t> boost::shared_ptr<data_t> Ark3DModel::FetchConstraint(unsigned id)
+{
+	ConstraintEquationBasePointer temp;
+	boost::shared_ptr<data_t> result;
+
+	std::map<unsigned,ConstraintEquationBasePointer>::iterator constraint_it = constraint_equation_list_.find(id);
+	if(constraint_it != constraint_equation_list_.end())
+	{
+		// primitive already exists
+		temp = constraint_it->second;
+
+	} else {
+		// primitive object does not exist, need to create it from the database
+		temp = ConstraintFactory(id);
+		AddConstraintEquation(temp);
+	}
+
+	// attempt to cast the PrimitiveBasePointer to the desired datatype, generate an error if the cast fails
+	if(dynamic_cast<data_t *>(temp.get()) != 0){
+		result = boost::dynamic_pointer_cast<data_t>(temp);
+	} else {
+		throw Ark3DException("Requested data type does not match database data type.");
+	}
+	
+	return result;
+}
+
+
 #endif //Ark3DModelH
