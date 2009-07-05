aea7b84268357b718edf7c5c8aa6b6da082eb450
michael.greminger <michael.greminger@26dfb9b6-6b4c-0410-b2d4-4f28753049b9>
2009-07-05 04:20:53 +0000

    ConstraintEquations.cpp now compiles.

--- trunk/src/ConstraintSolver/ConstraintEquations.h
+++ trunk/src/ConstraintSolver/ConstraintEquations.h
@@ -7,12 +7,12 @@ DECLARE_FUNCTION_5P(distance_point_2d_partial_2)
 DECLARE_FUNCTION_5P(distance_point_2d_partial_3)
 DECLARE_FUNCTION_5P(distance_point_2d_partial_4)
 
-static ex distance_point_2d_evalf(const ex & point1s, const ex & point1t, const ex & point2s, const ex & point2t, const ex & distance);
-static ex distance_point_2d_derivative(const ex & point1s, const ex & point1t, const ex & point2s, const ex & point2t, const ex & distance, unsigned diff_parm);
-static ex distance_point_2d_partial_0_evalf(const ex & point1s, const ex & point1t, const ex & point2s, const ex & point2t, const ex & distance);
-static ex distance_point_2d_partial_1_evalf(const ex & point1s, const ex & point1t, const ex & point2s, const ex & point2t, const ex & distance);
-static ex distance_point_2d_partial_2_evalf(const ex & point1s, const ex & point1t, const ex & point2s, const ex & point2t, const ex & distance);
-static ex distance_point_2d_partial_3_evalf(const ex & point1s, const ex & point1t, const ex & point2s, const ex & point2t, const ex & distance);
-static ex distance_point_2d_partial_4_evalf(const ex & point1s, const ex & point1t, const ex & point2s, const ex & point2t, const ex & distance);
+static GiNaC::ex distance_point_2d_evalf(const GiNaC::ex & point1s, const GiNaC::ex & point1t, const GiNaC::ex & point2s, const GiNaC::ex & point2t, const GiNaC::ex & distance);
+static GiNaC::ex distance_point_2d_derivative(const GiNaC::ex & point1s, const GiNaC::ex & point1t, const GiNaC::ex & point2s, const GiNaC::ex & point2t, const GiNaC::ex & distance, unsigned diff_parm);
+static GiNaC::ex distance_point_2d_partial_0_evalf(const GiNaC::ex & point1s, const GiNaC::ex & point1t, const GiNaC::ex & point2s, const GiNaC::ex & point2t, const GiNaC::ex & distance);
+static GiNaC::ex distance_point_2d_partial_1_evalf(const GiNaC::ex & point1s, const GiNaC::ex & point1t, const GiNaC::ex & point2s, const GiNaC::ex & point2t, const GiNaC::ex & distance);
+static GiNaC::ex distance_point_2d_partial_2_evalf(const GiNaC::ex & point1s, const GiNaC::ex & point1t, const GiNaC::ex & point2s, const GiNaC::ex & point2t, const GiNaC::ex & distance);
+static GiNaC::ex distance_point_2d_partial_3_evalf(const GiNaC::ex & point1s, const GiNaC::ex & point1t, const GiNaC::ex & point2s, const GiNaC::ex & point2t, const GiNaC::ex & distance);
+static GiNaC::ex distance_point_2d_partial_4_evalf(const GiNaC::ex & point1s, const GiNaC::ex & point1t, const GiNaC::ex & point2s, const GiNaC::ex & point2t, const GiNaC::ex & distance);
 
 
