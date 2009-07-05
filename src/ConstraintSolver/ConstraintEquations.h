27c2ea994ca42f05070cef5e9fab13728e5a9c44
michael.greminger <michael.greminger@26dfb9b6-6b4c-0410-b2d4-4f28753049b9>
2009-07-05 04:11:32 +0000

    Added function prototypes to ConstraintEquations.h and upgraded to a recent version of sympy to fix the code generated in ConstraintEquations.cpp.

--- src/ConstraintSolver/ConstraintEquations.h
+++ src/ConstraintSolver/ConstraintEquations.h
@@ -7,3 +7,12 @@ DECLARE_FUNCTION_5P(distance_point_2d_partial_2)
 DECLARE_FUNCTION_5P(distance_point_2d_partial_3)
 DECLARE_FUNCTION_5P(distance_point_2d_partial_4)
 
+static ex distance_point_2d_evalf(const ex & point1s, const ex & point1t, const ex & point2s, const ex & point2t, const ex & distance);
+static ex distance_point_2d_derivative(const ex & point1s, const ex & point1t, const ex & point2s, const ex & point2t, const ex & distance, unsigned diff_parm);
+static ex distance_point_2d_partial_0_evalf(const ex & point1s, const ex & point1t, const ex & point2s, const ex & point2t, const ex & distance);
+static ex distance_point_2d_partial_1_evalf(const ex & point1s, const ex & point1t, const ex & point2s, const ex & point2t, const ex & distance);
+static ex distance_point_2d_partial_2_evalf(const ex & point1s, const ex & point1t, const ex & point2s, const ex & point2t, const ex & distance);
+static ex distance_point_2d_partial_3_evalf(const ex & point1s, const ex & point1t, const ex & point2s, const ex & point2t, const ex & distance);
+static ex distance_point_2d_partial_4_evalf(const ex & point1s, const ex & point1t, const ex & point2s, const ex & point2t, const ex & distance);
+
+
