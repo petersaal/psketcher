140f85cf9faa20381252e4cfad679413c521727e
michael.greminger <michael.greminger@26dfb9b6-6b4c-0410-b2d4-4f28753049b9>
2009-07-06 04:13:04 +0000

    Updated generate_constraint_functions.py. constraint_equations.txt can now have comments and temporary variable substitutions can be defined.

--- trunk/src/ConstraintSolver/generate_constraint_functions.py
+++ trunk/src/ConstraintSolver/generate_constraint_functions.py
@@ -5,6 +5,7 @@ ConstraintFunctions.h and ConstraintFunctions.cpp. The existing C++ files are de
 from sympy import sympify,symbols,ccode
 from string import split,strip,rstrip
 from mako.template import Template
+import re
 
 class ConstraintEquation:
     def __init__(self,line):
@@ -12,7 +13,21 @@ class ConstraintEquation:
     
     def parse_equation(self,line):
         """Takes one line from the constraint_equations.txt file and parses it. Defines the
-        following parameters (function_name, paramater_list, parameter_dictionary, expression). """
+        following parameters (function_name, paramater_list, expression). """
+        
+        # First, remove and parse any substitutions that were defined (substititutions
+        # are enclosed in square braces and immediately follow the parameter list and proceed
+        # the equals sign). 
+        substitution_list = None
+        match = re.search(r'\[(.+)\]',line)
+        if match is not None:        
+            substitution_string = match.group(1)
+            line = re.sub(r'\[.+\]','',line)  # remove the substitution list from the input line
+            substitution_list = list()
+            for substitution in split(substitution_string,","):
+                (lhs,rhs) = split(substitution,"=")
+                substitution_list.append((strip(lhs),strip(rhs)))
+            substitution_list.reverse() # substitution needs to happen in reverse order since substitutions my depend on those to the left in the input line
         
         (lhs,rhs) = split(line,"=")
         (function_name,parameter_list) = split(lhs,"(")
@@ -20,20 +35,24 @@ class ConstraintEquation:
         parameter_list = rstrip(strip(parameter_list),")")
         self.parameter_list = [strip(parameter) for parameter in split(parameter_list,",")]
         
-        self.parameter_dictionary = dict()
-        for parameter in self.parameter_list:
-            self.parameter_dictionary[parameter] = symbols(parameter,each_char=False)
+        self.expression = sympify(strip(rhs))
         
-        self.expression = sympify(strip(rhs),locals=self.parameter_dictionary)
+        if substitution_list is not None:
+            self.expression = self.expression.subs(substitution_list)
         
+
 if __name__ == "__main__":
     input_file = open('constraint_equations.txt')
     
     constraint_equation_list = list()
     
     for line in input_file:
-        if "=" in line:
+        # remove any comments from the input line (any text after and including # is deleted)
+        line = re.sub(r'#.*','',line)
+        if re.match(r'\w+\([\w\s,]+\).*=.*',line):  # check to make sure that the line at least looks like an equation definition
             constraint_equation_list.append(ConstraintEquation(line))
+        elif re.match(r'\S',line):  # warn the user if a line was skipped 
+            print "Warning: The following line was ignored: \n" + line  
 
     header_template = Template(filename='ConstraintEquations.h.template')
     header_file = open('ConstraintEquations.h','w')
