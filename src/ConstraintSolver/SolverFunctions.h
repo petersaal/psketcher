83a411d97f7a9470fbdf47d5a3709c9ff84c1edc
michael.greminger <michael.greminger@26dfb9b6-6b4c-0410-b2d4-4f28753049b9>
2009-07-17 02:37:36 +0000

    SolverFunctions.cpp completed.

--- src/ConstraintSolver/SolverFunctions.h
+++ src/ConstraintSolver/SolverFunctions.h
@@ -1,94 +1,59 @@
-#include <ginac/ginac.h>
+#ifndef SolverFunctionsH
+#define SolverFunctionsH
 
-DECLARE_FUNCTION_5P(distance_point_2d)
-DECLARE_FUNCTION_5P(distance_point_2d_partial_0)
-DECLARE_FUNCTION_5P(distance_point_2d_partial_1)
-DECLARE_FUNCTION_5P(distance_point_2d_partial_2)
-DECLARE_FUNCTION_5P(distance_point_2d_partial_3)
-DECLARE_FUNCTION_5P(distance_point_2d_partial_4)
-
-static GiNaC::ex distance_point_2d_evalf(const GiNaC::ex & point1s, const GiNaC::ex & point1t, const GiNaC::ex & point2s, const GiNaC::ex & point2t, const GiNaC::ex & distance);
-static GiNaC::ex distance_point_2d_derivative(const GiNaC::ex & point1s, const GiNaC::ex & point1t, const GiNaC::ex & point2s, const GiNaC::ex & point2t, const GiNaC::ex & distance, unsigned diff_parm);
-static GiNaC::ex distance_point_2d_partial_0_evalf(const GiNaC::ex & point1s, const GiNaC::ex & point1t, const GiNaC::ex & point2s, const GiNaC::ex & point2t, const GiNaC::ex & distance);
-static GiNaC::ex distance_point_2d_partial_1_evalf(const GiNaC::ex & point1s, const GiNaC::ex & point1t, const GiNaC::ex & point2s, const GiNaC::ex & point2t, const GiNaC::ex & distance);
-static GiNaC::ex distance_point_2d_partial_2_evalf(const GiNaC::ex & point1s, const GiNaC::ex & point1t, const GiNaC::ex & point2s, const GiNaC::ex & point2t, const GiNaC::ex & distance);
-static GiNaC::ex distance_point_2d_partial_3_evalf(const GiNaC::ex & point1s, const GiNaC::ex & point1t, const GiNaC::ex & point2s, const GiNaC::ex & point2t, const GiNaC::ex & distance);
-static GiNaC::ex distance_point_2d_partial_4_evalf(const GiNaC::ex & point1s, const GiNaC::ex & point1t, const GiNaC::ex & point2s, const GiNaC::ex & point2t, const GiNaC::ex & distance);
-DECLARE_FUNCTION_9P(angle_line_2d_interior)
-DECLARE_FUNCTION_9P(angle_line_2d_interior_partial_0)
-DECLARE_FUNCTION_9P(angle_line_2d_interior_partial_1)
-DECLARE_FUNCTION_9P(angle_line_2d_interior_partial_2)
-DECLARE_FUNCTION_9P(angle_line_2d_interior_partial_3)
-DECLARE_FUNCTION_9P(angle_line_2d_interior_partial_4)
-DECLARE_FUNCTION_9P(angle_line_2d_interior_partial_5)
-DECLARE_FUNCTION_9P(angle_line_2d_interior_partial_6)
-DECLARE_FUNCTION_9P(angle_line_2d_interior_partial_7)
-DECLARE_FUNCTION_9P(angle_line_2d_interior_partial_8)
-
-static GiNaC::ex angle_line_2d_interior_evalf(const GiNaC::ex & line1_point1s, const GiNaC::ex & line1_point1t, const GiNaC::ex & line1_point2s, const GiNaC::ex & line1_point2t, const GiNaC::ex & line2_point1s, const GiNaC::ex & line2_point1t, const GiNaC::ex & line2_point2s, const GiNaC::ex & line2_point2t, const GiNaC::ex & angle);
-static GiNaC::ex angle_line_2d_interior_derivative(const GiNaC::ex & line1_point1s, const GiNaC::ex & line1_point1t, const GiNaC::ex & line1_point2s, const GiNaC::ex & line1_point2t, const GiNaC::ex & line2_point1s, const GiNaC::ex & line2_point1t, const GiNaC::ex & line2_point2s, const GiNaC::ex & line2_point2t, const GiNaC::ex & angle, unsigned diff_parm);
-static GiNaC::ex angle_line_2d_interior_partial_0_evalf(const GiNaC::ex & line1_point1s, const GiNaC::ex & line1_point1t, const GiNaC::ex & line1_point2s, const GiNaC::ex & line1_point2t, const GiNaC::ex & line2_point1s, const GiNaC::ex & line2_point1t, const GiNaC::ex & line2_point2s, const GiNaC::ex & line2_point2t, const GiNaC::ex & angle);
-static GiNaC::ex angle_line_2d_interior_partial_1_evalf(const GiNaC::ex & line1_point1s, const GiNaC::ex & line1_point1t, const GiNaC::ex & line1_point2s, const GiNaC::ex & line1_point2t, const GiNaC::ex & line2_point1s, const GiNaC::ex & line2_point1t, const GiNaC::ex & line2_point2s, const GiNaC::ex & line2_point2t, const GiNaC::ex & angle);
-static GiNaC::ex angle_line_2d_interior_partial_2_evalf(const GiNaC::ex & line1_point1s, const GiNaC::ex & line1_point1t, const GiNaC::ex & line1_point2s, const GiNaC::ex & line1_point2t, const GiNaC::ex & line2_point1s, const GiNaC::ex & line2_point1t, const GiNaC::ex & line2_point2s, const GiNaC::ex & line2_point2t, const GiNaC::ex & angle);
-static GiNaC::ex angle_line_2d_interior_partial_3_evalf(const GiNaC::ex & line1_point1s, const GiNaC::ex & line1_point1t, const GiNaC::ex & line1_point2s, const GiNaC::ex & line1_point2t, const GiNaC::ex & line2_point1s, const GiNaC::ex & line2_point1t, const GiNaC::ex & line2_point2s, const GiNaC::ex & line2_point2t, const GiNaC::ex & angle);
-static GiNaC::ex angle_line_2d_interior_partial_4_evalf(const GiNaC::ex & line1_point1s, const GiNaC::ex & line1_point1t, const GiNaC::ex & line1_point2s, const GiNaC::ex & line1_point2t, const GiNaC::ex & line2_point1s, const GiNaC::ex & line2_point1t, const GiNaC::ex & line2_point2s, const GiNaC::ex & line2_point2t, const GiNaC::ex & angle);
-static GiNaC::ex angle_line_2d_interior_partial_5_evalf(const GiNaC::ex & line1_point1s, const GiNaC::ex & line1_point1t, const GiNaC::ex & line1_point2s, const GiNaC::ex & line1_point2t, const GiNaC::ex & line2_point1s, const GiNaC::ex & line2_point1t, const GiNaC::ex & line2_point2s, const GiNaC::ex & line2_point2t, const GiNaC::ex & angle);
-static GiNaC::ex angle_line_2d_interior_partial_6_evalf(const GiNaC::ex & line1_point1s, const GiNaC::ex & line1_point1t, const GiNaC::ex & line1_point2s, const GiNaC::ex & line1_point2t, const GiNaC::ex & line2_point1s, const GiNaC::ex & line2_point1t, const GiNaC::ex & line2_point2s, const GiNaC::ex & line2_point2t, const GiNaC::ex & angle);
-static GiNaC::ex angle_line_2d_interior_partial_7_evalf(const GiNaC::ex & line1_point1s, const GiNaC::ex & line1_point1t, const GiNaC::ex & line1_point2s, const GiNaC::ex & line1_point2t, const GiNaC::ex & line2_point1s, const GiNaC::ex & line2_point1t, const GiNaC::ex & line2_point2s, const GiNaC::ex & line2_point2t, const GiNaC::ex & angle);
-static GiNaC::ex angle_line_2d_interior_partial_8_evalf(const GiNaC::ex & line1_point1s, const GiNaC::ex & line1_point1t, const GiNaC::ex & line1_point2s, const GiNaC::ex & line1_point2t, const GiNaC::ex & line2_point1s, const GiNaC::ex & line2_point1t, const GiNaC::ex & line2_point2s, const GiNaC::ex & line2_point2t, const GiNaC::ex & angle);
-DECLARE_FUNCTION_9P(angle_line_2d_exterior)
-DECLARE_FUNCTION_9P(angle_line_2d_exterior_partial_0)
-DECLARE_FUNCTION_9P(angle_line_2d_exterior_partial_1)
-DECLARE_FUNCTION_9P(angle_line_2d_exterior_partial_2)
-DECLARE_FUNCTION_9P(angle_line_2d_exterior_partial_3)
-DECLARE_FUNCTION_9P(angle_line_2d_exterior_partial_4)
-DECLARE_FUNCTION_9P(angle_line_2d_exterior_partial_5)
-DECLARE_FUNCTION_9P(angle_line_2d_exterior_partial_6)
-DECLARE_FUNCTION_9P(angle_line_2d_exterior_partial_7)
-DECLARE_FUNCTION_9P(angle_line_2d_exterior_partial_8)
-
-static GiNaC::ex angle_line_2d_exterior_evalf(const GiNaC::ex & line1_point1s, const GiNaC::ex & line1_point1t, const GiNaC::ex & line1_point2s, const GiNaC::ex & line1_point2t, const GiNaC::ex & line2_point1s, const GiNaC::ex & line2_point1t, const GiNaC::ex & line2_point2s, const GiNaC::ex & line2_point2t, const GiNaC::ex & angle);
-static GiNaC::ex angle_line_2d_exterior_derivative(const GiNaC::ex & line1_point1s, const GiNaC::ex & line1_point1t, const GiNaC::ex & line1_point2s, const GiNaC::ex & line1_point2t, const GiNaC::ex & line2_point1s, const GiNaC::ex & line2_point1t, const GiNaC::ex & line2_point2s, const GiNaC::ex & line2_point2t, const GiNaC::ex & angle, unsigned diff_parm);
-static GiNaC::ex angle_line_2d_exterior_partial_0_evalf(const GiNaC::ex & line1_point1s, const GiNaC::ex & line1_point1t, const GiNaC::ex & line1_point2s, const GiNaC::ex & line1_point2t, const GiNaC::ex & line2_point1s, const GiNaC::ex & line2_point1t, const GiNaC::ex & line2_point2s, const GiNaC::ex & line2_point2t, const GiNaC::ex & angle);
-static GiNaC::ex angle_line_2d_exterior_partial_1_evalf(const GiNaC::ex & line1_point1s, const GiNaC::ex & line1_point1t, const GiNaC::ex & line1_point2s, const GiNaC::ex & line1_point2t, const GiNaC::ex & line2_point1s, const GiNaC::ex & line2_point1t, const GiNaC::ex & line2_point2s, const GiNaC::ex & line2_point2t, const GiNaC::ex & angle);
-static GiNaC::ex angle_line_2d_exterior_partial_2_evalf(const GiNaC::ex & line1_point1s, const GiNaC::ex & line1_point1t, const GiNaC::ex & line1_point2s, const GiNaC::ex & line1_point2t, const GiNaC::ex & line2_point1s, const GiNaC::ex & line2_point1t, const GiNaC::ex & line2_point2s, const GiNaC::ex & line2_point2t, const GiNaC::ex & angle);
-static GiNaC::ex angle_line_2d_exterior_partial_3_evalf(const GiNaC::ex & line1_point1s, const GiNaC::ex & line1_point1t, const GiNaC::ex & line1_point2s, const GiNaC::ex & line1_point2t, const GiNaC::ex & line2_point1s, const GiNaC::ex & line2_point1t, const GiNaC::ex & line2_point2s, const GiNaC::ex & line2_point2t, const GiNaC::ex & angle);
-static GiNaC::ex angle_line_2d_exterior_partial_4_evalf(const GiNaC::ex & line1_point1s, const GiNaC::ex & line1_point1t, const GiNaC::ex & line1_point2s, const GiNaC::ex & line1_point2t, const GiNaC::ex & line2_point1s, const GiNaC::ex & line2_point1t, const GiNaC::ex & line2_point2s, const GiNaC::ex & line2_point2t, const GiNaC::ex & angle);
-static GiNaC::ex angle_line_2d_exterior_partial_5_evalf(const GiNaC::ex & line1_point1s, const GiNaC::ex & line1_point1t, const GiNaC::ex & line1_point2s, const GiNaC::ex & line1_point2t, const GiNaC::ex & line2_point1s, const GiNaC::ex & line2_point1t, const GiNaC::ex & line2_point2s, const GiNaC::ex & line2_point2t, const GiNaC::ex & angle);
-static GiNaC::ex angle_line_2d_exterior_partial_6_evalf(const GiNaC::ex & line1_point1s, const GiNaC::ex & line1_point1t, const GiNaC::ex & line1_point2s, const GiNaC::ex & line1_point2t, const GiNaC::ex & line2_point1s, const GiNaC::ex & line2_point1t, const GiNaC::ex & line2_point2s, const GiNaC::ex & line2_point2t, const GiNaC::ex & angle);
-static GiNaC::ex angle_line_2d_exterior_partial_7_evalf(const GiNaC::ex & line1_point1s, const GiNaC::ex & line1_point1t, const GiNaC::ex & line1_point2s, const GiNaC::ex & line1_point2t, const GiNaC::ex & line2_point1s, const GiNaC::ex & line2_point1t, const GiNaC::ex & line2_point2s, const GiNaC::ex & line2_point2t, const GiNaC::ex & angle);
-static GiNaC::ex angle_line_2d_exterior_partial_8_evalf(const GiNaC::ex & line1_point1s, const GiNaC::ex & line1_point1t, const GiNaC::ex & line1_point2s, const GiNaC::ex & line1_point2t, const GiNaC::ex & line2_point1s, const GiNaC::ex & line2_point1t, const GiNaC::ex & line2_point2s, const GiNaC::ex & line2_point2t, const GiNaC::ex & angle);
-DECLARE_FUNCTION_4P(tangent_edge_2d)
-DECLARE_FUNCTION_4P(tangent_edge_2d_partial_0)
-DECLARE_FUNCTION_4P(tangent_edge_2d_partial_1)
-DECLARE_FUNCTION_4P(tangent_edge_2d_partial_2)
-DECLARE_FUNCTION_4P(tangent_edge_2d_partial_3)
-
-static GiNaC::ex tangent_edge_2d_evalf(const GiNaC::ex & s1, const GiNaC::ex & t1, const GiNaC::ex & s2, const GiNaC::ex & t2);
-static GiNaC::ex tangent_edge_2d_derivative(const GiNaC::ex & s1, const GiNaC::ex & t1, const GiNaC::ex & s2, const GiNaC::ex & t2, unsigned diff_parm);
-static GiNaC::ex tangent_edge_2d_partial_0_evalf(const GiNaC::ex & s1, const GiNaC::ex & t1, const GiNaC::ex & s2, const GiNaC::ex & t2);
-static GiNaC::ex tangent_edge_2d_partial_1_evalf(const GiNaC::ex & s1, const GiNaC::ex & t1, const GiNaC::ex & s2, const GiNaC::ex & t2);
-static GiNaC::ex tangent_edge_2d_partial_2_evalf(const GiNaC::ex & s1, const GiNaC::ex & t1, const GiNaC::ex & s2, const GiNaC::ex & t2);
-static GiNaC::ex tangent_edge_2d_partial_3_evalf(const GiNaC::ex & s1, const GiNaC::ex & t1, const GiNaC::ex & s2, const GiNaC::ex & t2);
-DECLARE_FUNCTION_8P(parallel_line_2d)
-DECLARE_FUNCTION_8P(parallel_line_2d_partial_0)
-DECLARE_FUNCTION_8P(parallel_line_2d_partial_1)
-DECLARE_FUNCTION_8P(parallel_line_2d_partial_2)
-DECLARE_FUNCTION_8P(parallel_line_2d_partial_3)
-DECLARE_FUNCTION_8P(parallel_line_2d_partial_4)
-DECLARE_FUNCTION_8P(parallel_line_2d_partial_5)
-DECLARE_FUNCTION_8P(parallel_line_2d_partial_6)
-DECLARE_FUNCTION_8P(parallel_line_2d_partial_7)
-
-static GiNaC::ex parallel_line_2d_evalf(const GiNaC::ex & line1_point1s, const GiNaC::ex & line1_point1t, const GiNaC::ex & line1_point2s, const GiNaC::ex & line1_point2t, const GiNaC::ex & line2_point1s, const GiNaC::ex & line2_point1t, const GiNaC::ex & line2_point2s, const GiNaC::ex & line2_point2t);
-static GiNaC::ex parallel_line_2d_derivative(const GiNaC::ex & line1_point1s, const GiNaC::ex & line1_point1t, const GiNaC::ex & line1_point2s, const GiNaC::ex & line1_point2t, const GiNaC::ex & line2_point1s, const GiNaC::ex & line2_point1t, const GiNaC::ex & line2_point2s, const GiNaC::ex & line2_point2t, unsigned diff_parm);
-static GiNaC::ex parallel_line_2d_partial_0_evalf(const GiNaC::ex & line1_point1s, const GiNaC::ex & line1_point1t, const GiNaC::ex & line1_point2s, const GiNaC::ex & line1_point2t, const GiNaC::ex & line2_point1s, const GiNaC::ex & line2_point1t, const GiNaC::ex & line2_point2s, const GiNaC::ex & line2_point2t);
-static GiNaC::ex parallel_line_2d_partial_1_evalf(const GiNaC::ex & line1_point1s, const GiNaC::ex & line1_point1t, const GiNaC::ex & line1_point2s, const GiNaC::ex & line1_point2t, const GiNaC::ex & line2_point1s, const GiNaC::ex & line2_point1t, const GiNaC::ex & line2_point2s, const GiNaC::ex & line2_point2t);
-static GiNaC::ex parallel_line_2d_partial_2_evalf(const GiNaC::ex & line1_point1s, const GiNaC::ex & line1_point1t, const GiNaC::ex & line1_point2s, const GiNaC::ex & line1_point2t, const GiNaC::ex & line2_point1s, const GiNaC::ex & line2_point1t, const GiNaC::ex & line2_point2s, const GiNaC::ex & line2_point2t);
-static GiNaC::ex parallel_line_2d_partial_3_evalf(const GiNaC::ex & line1_point1s, const GiNaC::ex & line1_point1t, const GiNaC::ex & line1_point2s, const GiNaC::ex & line1_point2t, const GiNaC::ex & line2_point1s, const GiNaC::ex & line2_point1t, const GiNaC::ex & line2_point2s, const GiNaC::ex & line2_point2t);
-static GiNaC::ex parallel_line_2d_partial_4_evalf(const GiNaC::ex & line1_point1s, const GiNaC::ex & line1_point1t, const GiNaC::ex & line1_point2s, const GiNaC::ex & line1_point2t, const GiNaC::ex & line2_point1s, const GiNaC::ex & line2_point1t, const GiNaC::ex & line2_point2s, const GiNaC::ex & line2_point2t);
-static GiNaC::ex parallel_line_2d_partial_5_evalf(const GiNaC::ex & line1_point1s, const GiNaC::ex & line1_point1t, const GiNaC::ex & line1_point2s, const GiNaC::ex & line1_point2t, const GiNaC::ex & line2_point1s, const GiNaC::ex & line2_point1t, const GiNaC::ex & line2_point2s, const GiNaC::ex & line2_point2t);
-static GiNaC::ex parallel_line_2d_partial_6_evalf(const GiNaC::ex & line1_point1s, const GiNaC::ex & line1_point1t, const GiNaC::ex & line1_point2s, const GiNaC::ex & line1_point2t, const GiNaC::ex & line2_point1s, const GiNaC::ex & line2_point1t, const GiNaC::ex & line2_point2s, const GiNaC::ex & line2_point2t);
-static GiNaC::ex parallel_line_2d_partial_7_evalf(const GiNaC::ex & line1_point1s, const GiNaC::ex & line1_point1t, const GiNaC::ex & line1_point2s, const GiNaC::ex & line1_point2t, const GiNaC::ex & line2_point1s, const GiNaC::ex & line2_point1t, const GiNaC::ex & line2_point2s, const GiNaC::ex & line2_point2t);
+#include "SolverFunctionsBase.h"
 
 
+class distance_point_2d: public SolverFunctionsBase
+{
+    public:
+         distance_point_2d(DOFPointer point1s, DOFPointer point1t, DOFPointer point2s, DOFPointer point2t, DOFPointer distance);
+
+        double GetValue() const;
+        double GetValueSelf(const mmcMatrix &params) const;
+        mmcMatrix GetGradientSelf(const mmcMatrix &params) const;
+};
+
+class angle_line_2d_interior: public SolverFunctionsBase
+{
+    public:
+         angle_line_2d_interior(DOFPointer line1_point1s, DOFPointer line1_point1t, DOFPointer line1_point2s, DOFPointer line1_point2t, DOFPointer line2_point1s, DOFPointer line2_point1t, DOFPointer line2_point2s, DOFPointer line2_point2t, DOFPointer angle);
+
+        double GetValue() const;
+        double GetValueSelf(const mmcMatrix &params) const;
+        mmcMatrix GetGradientSelf(const mmcMatrix &params) const;
+};
+
+class angle_line_2d_exterior: public SolverFunctionsBase
+{
+    public:
+         angle_line_2d_exterior(DOFPointer line1_point1s, DOFPointer line1_point1t, DOFPointer line1_point2s, DOFPointer line1_point2t, DOFPointer line2_point1s, DOFPointer line2_point1t, DOFPointer line2_point2s, DOFPointer line2_point2t, DOFPointer angle);
+
+        double GetValue() const;
+        double GetValueSelf(const mmcMatrix &params) const;
+        mmcMatrix GetGradientSelf(const mmcMatrix &params) const;
+};
+
+class tangent_edge_2d: public SolverFunctionsBase
+{
+    public:
+         tangent_edge_2d(DOFPointer s1, DOFPointer t1, DOFPointer s2, DOFPointer t2);
+
+        double GetValue() const;
+        double GetValueSelf(const mmcMatrix &params) const;
+        mmcMatrix GetGradientSelf(const mmcMatrix &params) const;
+};
+
+class parallel_line_2d: public SolverFunctionsBase
+{
+    public:
+         parallel_line_2d(DOFPointer line1_point1s, DOFPointer line1_point1t, DOFPointer line1_point2s, DOFPointer line1_point2t, DOFPointer line2_point1s, DOFPointer line2_point1t, DOFPointer line2_point2s, DOFPointer line2_point2t);
+
+        double GetValue() const;
+        double GetValueSelf(const mmcMatrix &params) const;
+        mmcMatrix GetGradientSelf(const mmcMatrix &params) const;
+};
+
+
+
+#endif //SolverFunctionsH
